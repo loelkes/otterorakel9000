aa1ffe18d5b4d623aeed912327a5a09f4af295ef
loelkes <christian.loelkes@gmail.com>
2018-09-27 12:45:01 +0200

    CORS

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -2,13 +2,13 @@ from config import FlaskConfig
 
 from flask import Flask
 from flask_bootstrap import Bootstrap
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+from flask_cors import CORS
 
 from questions import Questions
 
 app = Flask(__name__)
 app.config.from_object(FlaskConfig)
+CORS(app)
 # db = SQLAlchemy(app)
 # migrate = Migrate(app, db)
 
