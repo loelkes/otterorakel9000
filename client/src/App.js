d58ab9a2106d2a5d3cbc0f3ae11c7b3fc4c4a8b8
Kolja Esders <kolja.esders@gmail.com>
2018-09-27 16:27:30 +0200

    Using react-router for routes

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,19 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Home from './components/Home';
+import DashboardPage from './components/DashboardPage';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 class App extends Component {
   render() {
-    return <Home />;
+    return (
+      <Router>
+        <div>
+          <Route exact path="/" component={Home} />
+          <Route path="/dashboard" component={DashboardPage} />
+        </div>
+      </Router>
+    );
   };
 }
 
